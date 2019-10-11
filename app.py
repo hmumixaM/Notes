42caa8840057cb90e5c9c6e13fa5ad5e54e7f439
hmumixaM <max@12450.top>
2019-10-10 23:13:53 -0400

    Flask Tests

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/<path:url>')
+def name(url):
+    return 'Url: {}'.format(url)
+
+
+@app.route('/about')
+def about():
+    return 'About the program'
+
+
+@app.route('/404')
+def error404():
+    return '404'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
